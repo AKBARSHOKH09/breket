f26392722561805100ea252f6cb70f161294bdb9
AKBARSHOKH09 <muhammadjonovakbar073@gmail.com>
2024-11-25 16:29:55 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+ Math.round(Math.random() * 10)  ( "random son chiqaradi" );
+ Math.round(1.4)   ("qoldiq sonni butunlab beradi") ;
+ Math.floor(24.9)   ("qoldiq sonni yaxlitlab beradi pastga qarab") ;
+ Math.ceil(-22.9)    ("qoldiq sonni tepaga yaxlatibg beradi") ;
+ Math.max(100, 50 ,101)  ("eng katta sonni chqarip beradi ") ;
+ Math.min(100, 50 ,101)   ("minimum sonni chqarib beradi")  ;
+ Math.sqrt(100)   ("darajasini aniqlab berado") ;
